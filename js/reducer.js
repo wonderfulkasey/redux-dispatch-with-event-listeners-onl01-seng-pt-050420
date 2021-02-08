bd4cd490841ba701bc8c12467b4fb439f596b687
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-08 13:16:13 +0000

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,30 @@
 // add code snippets from README
+
+let state;
+
+function reducer(state = {count: 0 }, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+} 
+
+function dispatch(action){
+    state = reducer(state, action);
+    render()
+  } 
+
+function render(){
+    let container = document.getElementById('container');
+    container.textContent = state.count; 
+} 
+
+dispatch({type: '@@INIT'}) 
+
+let button = document.getElementById('button'); //grab the button
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+}) 
